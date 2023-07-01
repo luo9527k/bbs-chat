c72a07ac41f4e162219d187c9839daff1639973a
luowu <3396827568@qq.com>
2023-07-01 18:52:33 +0800

    change msg

--- src/utils/request.js
+++ src/utils/request.js
@@ -1,11 +1,3 @@
-/*
- * @Author: SatanYoung amgyjl0926@163.com
- * @Date: 2023-04-14 14:20:46
- * @LastEditors: SatanYoung amgyjl0926@163.com
- * @LastEditTime: 2023-04-19 11:36:47
- * @FilePath: \easybbs-web\src\utils\request.js
- * @Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
- */
 import axios from 'axios';
 import Message from '@/utils/message';
 
--- src/utils/verify.js
+++ src/utils/verify.js
@@ -1,11 +1,3 @@
-/*
- * @Author: SatanYoung amgyjl0926@163.com
- * @Date: 2023-04-12 19:11:39
- * @LastEditors: SatanYoung amgyjl0926@163.com
- * @LastEditTime: 2023-04-12 21:22:07
- * @FilePath: \easybbs-web\src\utils\verify.js
- * @Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
- */
 const regs = {
     email: /^[a-zA-Z0-9]+([-_.][a-zA-Z0-9]+)*@[a-zA-Z0-9]+([-_.][a-zA-Z0-9]+)*\.[a-z]{2,}$/,
     number: /^([0][1-9][0-9]*)$/,
